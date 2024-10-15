fdd6990f257f7a810f26603058f292e44e8a9510
Luis <a01722501@tec.mx>
2024-10-15 13:45:13 -0600

    cambios backend Citas

--- backEnd/schemas/citasSchema.js
+++ backEnd/schemas/citasSchema.js
@@ -4,11 +4,7 @@ const citasSchema = new mongoose.Schema({
     id: { type: String, required: true },
     cliente_id: { type: String, required: true },
     abogado_id: { type: String, required: true },
-    plataforma: { type: String, required: true },
-    estado: { type: String, required: true },
-    enlace_invitacion: { type: String, required: false },
-    horaComienzo: { type: Date, required: true },
-    horaFin: { type: Date, required: true },
+    hora: { type: Date, required: true },
 },{
     collection: 'citas'
 });
